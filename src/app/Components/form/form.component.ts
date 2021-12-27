dce525b11d4be407360c283a61135535767ff736
Oliver <oliver.mwangale@moringaschool.com>
2021-12-27 08:42:50 +0300

    add form compoent that takes in the inputs of the user from the form

--- src/app/Components/form/form.component.ts
+++ src/app/Components/form/form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Data } from 'src/app/Class/data';
 
 @Component({
   selector: 'app-form',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  newQuote = new Data(0,"","",new Date())
+  
+  @Output() addQuote:any = new EventEmitter<Data>();
+
+  submitQuote(){
+    // var quoteLength = this.newQuote.length + 1;
+    this.addQuote.emit(this.newQuote)
+  }
 
   ngOnInit(): void {
   }
