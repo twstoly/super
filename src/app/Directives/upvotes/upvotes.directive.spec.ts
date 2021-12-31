8f793bf8730dd038b5baaf42f9c4cecbadb8b045
Oliver <oliver.mwangale@moringaschool.com>
2021-12-31 23:33:42 +0300

    Auto-generated commit [ci skip]

--- src/app/Directives/upvotes/upvotes.directive.spec.ts
+++
@@ -1,9 +0,0 @@
-import { NONE_TYPE } from '@angular/compiler';
-import { UpvotesDirective } from './upvotes.directive';
-
-describe('UpvotesDirective', () => {
-  it('should create an instance', () => {
-    const directive = new UpvotesDirective();
-    expect(directive).toBeTruthy();
-  });
-});
